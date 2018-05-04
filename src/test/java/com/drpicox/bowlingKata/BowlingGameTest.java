5c766169cfa1d49e8a6329ab44ab543686321178
Axel <axelrosp@gmail.com>
2018-05-04 09:09:19 +0200

    Slide 33

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -6,27 +6,40 @@ import org.junit.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class BowlingGameTest {
+    private Game g;
+
+    protected void setUp() throws Exception{
+        g = new Game();
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i=0; i<n; i++){
+            g.roll(pins);
+        }
+    }
+
     @Test
     public void gutterGame() throws Exception{
-        //Test roll game
-       Game g = new Game();
-       for (int i=0; i<20; i++){
-           g.roll(0);
-       }
+       rollMany(20, 0);
        assertThat(g.score(), is(0));
     }
 
     @Test
     public void testAllOnes() throws Exception{
-        //Test roll game
-        Game g = new Game();
-        for (int i=0; i<20; i++){
-            g.roll(1);
-        }
+        rollMany(20, 1);
         assertThat(g.score(), is(20));
     }
+
+ /*   public void testOneSpare() throws Exception{
+        g.roll(5);
+        g.roll(5); //spare
+        g.roll(3);
+        rollMany(17,0);
+        assertEquals(g.score(), 16);
+    }*/
 
 }
