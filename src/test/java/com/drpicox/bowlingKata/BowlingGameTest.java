d54be696668270ec95f0b1c828c25c5d60b62b11
Axel <axelrosp@gmail.com>
2018-05-04 09:16:04 +0200

    Slide 34

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -12,7 +12,8 @@ import static org.junit.Assert.assertThat;
 public class BowlingGameTest {
     private Game g;
 
-    protected void setUp() throws Exception{
+    @Before
+    public void setUp() throws Exception{
         g = new Game();
     }
 
@@ -39,7 +40,7 @@ public class BowlingGameTest {
         g.roll(5); //spare
         g.roll(3);
         rollMany(17,0);
-        assertEquals(g.score(), 16);
+        assertThat(g.score(), 16);
     }*/
 
 }
